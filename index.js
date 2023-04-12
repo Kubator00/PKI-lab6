c96aa20fb3b30e9aecbaa15d3e0703cccac55a06
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2023-04-12 16:21:22 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
@@ -9,6 +9,8 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -16,6 +18,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+app.listen(process.env.PORT || 80, () => {
+    console.log(`Server is listening`)
 })
